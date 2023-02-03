19da3a9f9ee824657dfb6c5837d9e59c75a71832
Purushottam <puru.shiw2@gmail.com>
2023-02-04 00:45:11 +0545

    Done it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './App.css'
 
 function App() {
+  const[tmp,setTmp]=useState("");
+  const[value,setValue]=useState([]);
+
+ const handleOnCHange=(e)=>{
+    setTmp(e.target.value)
+  }
+  const handleOnClick=()=>{
+    setValue((goat)=>{
+      return [...goat,tmp]
+
+    })
+    setTmp("")
+
+
+  }
+  const handleOnDelete=(index)=>{
+    setValue(prev => prev.filter((arrElement, i) => i !== index));
+  };
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <h1>ToDo List</h1>
+    <input type="text" placeholder="What you want to do " value={tmp} onChange={handleOnCHange} />
+    <button onClick={handleOnClick}>+</button>
+    
+    {
+      value.map((rank, index)=>{
+        return(
+       <>
+
+         <p>{rank}  <ion-icon name="close-outline" onClick={()=>handleOnDelete(index)}></ion-icon></p>
+          
+         
+         </>
+         
+          
+        ) 
+      })
+    }
+    </div>
+    
+    
+
     </div>
   );
 }
